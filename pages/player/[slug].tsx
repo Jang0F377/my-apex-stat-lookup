f7b678cc39d049c6a53611a901a77ecfb48b8477
Matt Garrett <mjgarrett7092@gmail.com>
2024-01-18 11:47:49 -0800

    feat: updating api endpoints

--- pages/player/[slug].tsx
+++ pages/player/[slug].tsx
@@ -7,71 +7,71 @@ import GlobalStats from "../../components/GlobalStats";
 import AllLegends from "../../components/AllLegends";
 
 function Player() {
-  const router = useRouter();
-  const { system, gamertag } = router.query;
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [playerStats, setPlayerStats] = useState<ApiResponse>();
+	const router = useRouter();
+	const { system, gamertag } = router.query;
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+	const [playerStats, setPlayerStats] = useState<ApiResponse>();
 
-  const URL = `https://obscure-shelf-17700.herokuapp.com/https://api.mozambiquehe.re/bridge?player=${gamertag}&platform=${system}`;
+	const URL = `https://api.mozambiquehe.re/bridge?player=${gamertag}&platform=${system}`;
 
-  async function api(url: string) {
-    setLoading(true);
-    const headers: HeadersInit = {
-      Authorization: `${process.env.NEXT_PUBLIC_APEX_STAT_API}`,
-    };
-    const opts: RequestInit = {
-      method: "GET",
-      headers: headers,
-    };
-    return await fetch(url, opts)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setPlayerStats(data);
-      })
-      .then(() => setLoading(false))
-      .catch(() => setError(true));
-  }
+	async function api(url: string) {
+		setLoading(true);
+		const headers: HeadersInit = {
+			Authorization: `${process.env.NEXT_PUBLIC_APEX_STAT_API}`,
+		};
+		const opts: RequestInit = {
+			method: "GET",
+			headers: headers,
+		};
+		return await fetch(url, opts)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setPlayerStats(data);
+			})
+			.then(() => setLoading(false))
+			.catch(() => setError(true));
+	}
 
-  useEffect(() => {
-    api(URL)
-      .then()
-      .catch(() => setError(true));
-  }, []);
+	useEffect(() => {
+		api(URL)
+			.then()
+			.catch(() => setError(true));
+	}, []);
 
-  return (
-    <div className="">
-      {loading ? (
-        <div className="text-3xl lg:text-7xl text-bright-light-red text-center py-10 lg:py-0 lg:p-20 m-10 animate-pulse">
-          ...Loading
-        </div>
-      ) : error ? (
-        <div className="text-7xl text-bright-light-red text-center p-20 m-10 animate-pulse">
-          ...Error
-        </div>
-      ) : (
-        <div className="mx-auto max-w-7xl justify-center">
-          <section className="mt-2 mb-3">
-            <h1 className="text-3xl p-1.5  md:text-5xl text-black text-center md:p-5 font-medium tracking-wide">
-              {playerStats?.global.name}
-            </h1>
-            <Realtime realtime={playerStats?.realtime} />
-          </section>
-          <section className="flex justify-center my-3">
-            <SelectedLegend selected={playerStats?.legends.selected} />
-          </section>
-          <section>
-            <GlobalStats global={playerStats?.global} />
-          </section>
-          <section className="flex my-3 justify-center">
-            <AllLegends legends={playerStats?.legends.all} />
-          </section>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="">
+			{loading ? (
+				<div className="text-3xl lg:text-7xl text-bright-light-red text-center py-10 lg:py-0 lg:p-20 m-10 animate-pulse">
+					...Loading
+				</div>
+			) : error ? (
+				<div className="text-7xl text-bright-light-red text-center p-20 m-10 animate-pulse">
+					...Error
+				</div>
+			) : (
+				<div className="mx-auto max-w-7xl justify-center">
+					<section className="mt-2 mb-3">
+						<h1 className="text-3xl p-1.5  md:text-5xl text-black text-center md:p-5 font-medium tracking-wide">
+							{playerStats?.global.name}
+						</h1>
+						<Realtime realtime={playerStats?.realtime} />
+					</section>
+					<section className="flex justify-center my-3">
+						<SelectedLegend selected={playerStats?.legends.selected} />
+					</section>
+					<section>
+						<GlobalStats global={playerStats?.global} />
+					</section>
+					<section className="flex my-3 justify-center">
+						<AllLegends legends={playerStats?.legends.all} />
+					</section>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Player;
--- pages/rotations/crafter/index.tsx
+++ pages/rotations/crafter/index.tsx
@@ -3,86 +3,86 @@ import { CraftingRotationObject } from "../../../typings";
 import CrafterRotation from "../../../components/CrafterRotation";
 
 function Crafter() {
-  const [crafter, setCrafter] = useState<Array<CraftingRotationObject>>();
-  const [crafterLoading, setCrafterLoading] = useState(true);
-  const crafterUrl = `https://obscure-shelf-17700.herokuapp.com/https://api.mozambiquehe.re/crafting`;
+	const [crafter, setCrafter] = useState<Array<CraftingRotationObject>>();
+	const [crafterLoading, setCrafterLoading] = useState(true);
+	const crafterUrl = `https://api.mozambiquehe.re/crafting`;
 
-  async function getCrafterData(url: string) {
-    if (!crafterLoading) setCrafterLoading(true);
-    const headers: HeadersInit = {
-      Authorization: `${process.env.NEXT_PUBLIC_APEX_STAT_API}`,
-    };
-    const opts: RequestInit = {
-      method: "GET",
-      headers: headers,
-    };
+	async function getCrafterData(url: string) {
+		if (!crafterLoading) setCrafterLoading(true);
+		const headers: HeadersInit = {
+			Authorization: `${process.env.NEXT_PUBLIC_APEX_STAT_API}`,
+		};
+		const opts: RequestInit = {
+			method: "GET",
+			headers: headers,
+		};
 
-    return await fetch(url, opts)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setCrafter(data);
-      })
-      .then(() => setCrafterLoading(false))
-      .catch((err) => console.warn(err));
-  }
+		return await fetch(url, opts)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setCrafter(data);
+			})
+			.then(() => setCrafterLoading(false))
+			.catch((err) => console.warn(err));
+	}
 
-  useEffect(() => {
-    getCrafterData(crafterUrl)
-      .then()
-      .catch((err) => console.warn(err));
-  }, []);
+	useEffect(() => {
+		getCrafterData(crafterUrl)
+			.then()
+			.catch((err) => console.warn(err));
+	}, []);
 
-  return (
-    <div className="xl:max-w-7xl lg:max-w-6xl lg:mx-auto mt-5 mb-1.5 p-2">
-      {crafterLoading ? (
-        <div className="text-3xl lg:text-7xl text-bright-light-red text-center py-10 lg:py-0 lg:p-20 m-10 animate-pulse">
-          ...Loading
-        </div>
-      ) : (
-        <>
-          <h2 className="text-3xl lg:text-4xl text-center my-1 py-1 font-medium">
-            In the Crafter:
-          </h2>
-          <p className="text-sm text-center text-dark-red animate-pulse">
-            Data/Images provided by{" "}
-            <a
-              target="_blank"
-              href="https://apexlegendsstatus.com/"
-              rel="noreferrer"
-            >
-              Apex Legends Status
-            </a>
-          </p>
-          <div className="space-y-2 md:space-y-3 ">
-            {crafter?.map((x) =>
-              x.bundleType === "weekly" ? (
-                <CrafterRotation crafts={x?.bundleContent} />
-              ) : (
-                <div />
-              )
-            )}
+	return (
+		<div className="xl:max-w-7xl lg:max-w-6xl lg:mx-auto mt-5 mb-1.5 p-2">
+			{crafterLoading ? (
+				<div className="text-3xl lg:text-7xl text-bright-light-red text-center py-10 lg:py-0 lg:p-20 m-10 animate-pulse">
+					...Loading
+				</div>
+			) : (
+				<>
+					<h2 className="text-3xl lg:text-4xl text-center my-1 py-1 font-medium">
+						In the Crafter:
+					</h2>
+					<p className="text-sm text-center text-dark-red animate-pulse">
+						Data/Images provided by{" "}
+						<a
+							target="_blank"
+							href="https://apexlegendsstatus.com/"
+							rel="noreferrer"
+						>
+							Apex Legends Status
+						</a>
+					</p>
+					<div className="space-y-2 md:space-y-3 ">
+						{crafter?.map((x) =>
+							x.bundleType === "weekly" ? (
+								<CrafterRotation crafts={x?.bundleContent} />
+							) : (
+								<div />
+							)
+						)}
 
-            {crafter?.map((x) =>
-              x.bundleType === "daily" ? (
-                <CrafterRotation crafts={x?.bundleContent} />
-              ) : (
-                <div />
-              )
-            )}
-            {crafter?.map((x) =>
-              x.bundleType === "permanent" && x.bundle !== "ammo" ? (
-                <CrafterRotation crafts={x?.bundleContent} />
-              ) : (
-                <div />
-              )
-            )}
-          </div>
-        </>
-      )}
-    </div>
-  );
+						{crafter?.map((x) =>
+							x.bundleType === "daily" ? (
+								<CrafterRotation crafts={x?.bundleContent} />
+							) : (
+								<div />
+							)
+						)}
+						{crafter?.map((x) =>
+							x.bundleType === "permanent" && x.bundle !== "ammo" ? (
+								<CrafterRotation crafts={x?.bundleContent} />
+							) : (
+								<div />
+							)
+						)}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default Crafter;
